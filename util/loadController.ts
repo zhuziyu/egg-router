56a0507e8a456e38ca28ab10c32132e62ed4241b
zhuziyu <admin@zhuziyu.cn>
2020-02-26 19:55:22 +0800

    fix bug

--- util/loadController.ts
+++ util/loadController.ts
@@ -5,9 +5,21 @@ import { promises as fs } from 'fs';
 import * as path from 'path';
 
 export default async function(baseDir: string) {
-  const files = await fs.readdir(path.join(baseDir, './app/controller'));
+
+  let files = await fs.readdir(path.join(baseDir, './app/controller'));
+  files = files.map(file => {
+    const fileAry = file.split('.');
+    fileAry.pop();
+    return fileAry.join('.');
+  });
+  const uniqFiles: string[] = [];
+  files.forEach(file => {
+    if (!uniqFiles.includes(file)) uniqFiles.push(file);
+  });
+
   const controllers: any[] = [];
-  for (const file of files) {
+
+  for (const file of uniqFiles) {
     const exportObj = await import(path.join(baseDir, './app/controller', file));
     controllers.push(exportObj.default);
   }
